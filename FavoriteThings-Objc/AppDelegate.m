6650b9149b618df3f40cbde654b745b3d57fdf61
Peter Zimmerman <pzim10@gmail.com>
2015-06-25 19:42:16 -0600

    Create tableView and controllers

--- FavoriteThings-Objc/AppDelegate.m
+++ FavoriteThings-Objc/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "FavViewController.h"
 
 @interface AppDelegate ()
 
@@ -20,6 +21,9 @@
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:[FavViewController new]];
+    self.window.rootViewController = navController;
     return YES;
 }
 
---
+++ FavoriteThings-Objc/FavViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavViewController.h
+//  FavoriteThings-Objc
+//
+//  Created by Peter Zimmerman on 6/25/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavViewController : UIViewController
+
+@end
